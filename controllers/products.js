a9e546ac36ac4b1ee9dfab8bfba45276e71c571b
chuzksy-codeactive <chuzksy@gmail.com>
2020-04-20 19:14:32 +0100

    add rdifferent routes to the navigation and persist data

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProducts = (req, res) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product'
   });
@@ -16,7 +16,7 @@ exports.postAddProduct = (req, res) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop', {
+    res.render('shop/product-list', {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
