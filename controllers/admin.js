ab5fc5d1fd78fe4cc8445bd521cdd24c998630ad
chuzksy-codeactive <chuzksy@gmail.com>
2020-04-20 21:29:14 +0100

    storing product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,7 +8,12 @@ exports.getAddProducts = (req, res) => {
 }
 
 exports.postAddProduct = (req, res) => {
-  const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  
+  const product = new Product(title, imageUrl, description, price);
   product.save();
 
   res.redirect('/');
