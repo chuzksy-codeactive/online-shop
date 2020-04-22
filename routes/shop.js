71c8bd61d54b33a722c32e0e467dbbc358c50a8a
chuzksy-codeactive <chuzksy@gmail.com>
2020-04-22 06:06:19 +0100

    display cart items

--- routes/shop.js
+++ routes/shop.js
@@ -15,9 +15,10 @@ router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
-  
